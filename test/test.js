95a200b1de0f1c36057dee3ac1eb668a3437a6a9
opfl <openthefrog@gmail.com>
2014-03-13 19:56:38 -0700

    Small updates

--- test/test.js
+++ test/test.js
@@ -1,38 +1,34 @@
 var bter = require('../lib/bter');
 
 exports['get all ticket pairs'] = function(test) {
-	test.throws(function() { bter.getAllPairs(); });
-	test.throws(function() { bter.getAllPairs('all'); });
-	test.expect(2);
+	test.throws(function() { bter.getAllPairs(); }, Error, 'No callback defined');
+	test.expect(1);
     test.done();
 };
 
 exports['get ticker for btc/doge'] = function(test) {
-	test.throws(function() { bter.getTicker(); });
-	test.throws(function() { bter.getTicker('btc/doge'); });
-	test.throws(function() { bter.getTicker({ CURR_A: 'btc' }); });
-	test.throws(function() { bter.getTicker({ CURR_B: 'doge' }); });
-	test.throws(function() { bter.getTicker({ CURR_A: 'btc', CURR_B: 'doge' }); });
-	test.expect(5);
+	test.throws(function() { bter.getTicker(); }, Error, 'No callback defined');
+	test.throws(function() { bter.getTicker({}); }, Error, 'No options defined');
+	test.throws(function() { bter.getTicker({ CURR_A: 'btc' }); }, Error, 'You must define two currencies');
+	test.throws(function() { bter.getTicker({ CURR_B: 'doge' }); }, Error, 'You must define two currencies');
+	test.expect(4);
     test.done();
 };
 
 exports['get market depth for btc/doge'] = function(test) {
-	test.throws(function() { bter.getDepth(); });
-	test.throws(function() { bter.getDepth('btc/doge'); });
-	test.throws(function() { bter.getDepth({ CURR_A: 'btc' }); });
-	test.throws(function() { bter.getDepth({ CURR_B: 'doge' }); });
-	test.throws(function() { bter.getDepth({ CURR_A: 'btc', CURR_B: 'doge' }); });
-	test.expect(5);
+	test.throws(function() { bter.getDepth(); }, Error, 'No callback defined');
+	test.throws(function() { bter.getDepth({}); }, Error, 'No options defined');
+	test.throws(function() { bter.getDepth({ CURR_A: 'btc' }); }, Error, 'You must define two currencies');
+	test.throws(function() { bter.getDepth({ CURR_B: 'doge' }); }, Error, 'You must define two currencies');
+	test.expect(4);
     test.done();
 };
 
 exports['get trade history for btc/doge'] = function(test) {
-	test.throws(function() { bter.getHistory(); });
-	test.throws(function() { bter.getHistory('btc/doge'); });
-	test.throws(function() { bter.getHistory({ CURR_A: 'btc' }); });
-	test.throws(function() { bter.getHistory({ CURR_B: 'doge' }); });
-	test.throws(function() { bter.getHistory({ CURR_A: 'btc', CURR_B: 'doge' }); });
-	test.expect(5);
+	test.throws(function() { bter.getHistory(); }, Error, 'No callback defined');
+	test.throws(function() { bter.getHistory({}); }, Error, 'No options defined');
+	test.throws(function() { bter.getHistory({ CURR_A: 'btc' }); }, Error, 'You must define two currencies');
+	test.throws(function() { bter.getHistory({ CURR_B: 'doge' }); }, Error, 'You must define two currencies');
+	test.expect(4);
     test.done();
 };
